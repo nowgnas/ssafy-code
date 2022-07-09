7e488914deaa110524f045c0b419b8c85e9826ce
nowgnas <marshmello.sw@gmail.com>
2022-07-09 17:28:55 +0900

    단어들어가는문제 다시 보기

--- swea/1974_스도쿠검증.py
+++ swea/1974_스도쿠검증.py
@@ -1,4 +1,56 @@
 '''
 https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq&categoryId=AV5Psz16AYEDFAUq&categoryType=CODE&problemTitle=1974&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
 
+
+1
+7 7 6 4 2 9 5 8 1
+5 8 9 1 6 7 3 2 4
+2 1 4 5 8 3 6 9 7
+8 4 7 9 3 6 1 5 2
+1 5 3 8 4 2 9 7 6
+9 6 2 7 5 1 8 4 3
+4 2 1 3 9 8 7 6 5
+3 9 5 6 7 4 2 1 8
+6 7 8 2 1 5 4 3 9
 '''
+
+
+def solution():
+    result = 1
+    checkEachTable = []
+    checkVertical = [[0 for _ in range(9)] for _ in range(9)]
+    for idx1 in range(9):
+        # 가로 줄 체크
+        line = list(map(int, input().split()))
+        setLine = len(set(line)) == 9
+        if not setLine:
+            result = 0
+        checkEachTable.append(line)
+        for idx2 in range(9):
+            checkVertical[idx2][idx1] = line[idx2]
+
+    table = []
+    for loop in range(0, 7, 3):
+        for loop2 in range(0, 7, 3):
+            threeBythree = []
+            for loop3 in range(3):
+                for loop4 in range(3):
+                    threeBythree.append(
+                        checkEachTable[loop+loop3][loop2+loop4])
+            table.append(threeBythree)
+
+    for ver, tab in zip(checkVertical, table):
+        # 세로 줄 체크
+        lineCheck = len(set(ver)) == 9
+        lineCheck2 = len(set(tab)) == 9
+        if not lineCheck or not lineCheck2:
+            result = 0
+            break
+
+    return result
+
+
+n = int(input())
+for item in range(n):
+    result = solution()
+    print(f'#{item+1} {result}')
--- swea/prob1979_어디에단어가들어갈수있을까.py
+++ swea/prob1979_어디에단어가들어갈수있을까.py
@@ -2,8 +2,58 @@
 https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PuPq6AaQDFAUq&categoryId=AV5PuPq6AaQDFAUq&categoryType=CODE&problemTitle=1979&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
 
 
+
+2
+5 3
+0 0 1 1 1
+1 1 1 1 0
+0 0 1 0 0
+0 1 1 1 1
+1 1 1 0 1
+6 2
+1 0 0 1 0 1
+1 1 0 1 1 0
+1 0 1 1 1 1
+0 1 1 0 1 1
+0 1 1 1 0 0
+0 0 0 0 1 1
 '''
+
+
+def solution(n, k):
+    table = []
+    check = '1' * k
+    preZero = '0' + check
+    rearZero = check + '0'
+    bothZero = '0' + check + '0'
+    result = 0
+    verticalCheck = [[0 for _ in range(n)] for _ in range(n)]
+    for _ in range(n):
+        lst = list(map(str, input().split()))
+        table.append(lst)
+
+    for ver in range(n):
+        for hor in range(n):
+            verticalCheck[ver][hor] = table[hor][ver]
+
+    for line1, line2 in zip(table, verticalCheck):
+        lineToString = ''.join(line1)
+        lineToString2 = ''.join(line2)
+
+        if bothZero in lineToString:
+            result += lineToString.count(bothZero)
+        # 011과 110과 0110 구분 가능해야 함
+
+        if preZero in lineToString or rearZero in lineToString:
+            result += 1
+        if preZero in lineToString2 or rearZero in lineToString2:
+            result += 1
+
+    return result
+
 
 n = int(input())
 for test in range(n):
     n, k = map(int, input().split())
+    result = solution(n, k)
+    print(f'#{test+1} {result}')
